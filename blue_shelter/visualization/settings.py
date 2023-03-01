6ee945dd3127f046d904cf85ce0b64c521f300bb
yu-je0ng <106722166+yu-je0ng@users.noreply.github.com>
2023-03-02 01:56:33 +0900

    Fixed secretkey

--- blue_shelter/visualization/settings.py
+++ blue_shelter/visualization/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@^aq@gm@-+)2&f&@6brjd^*^$sl-c63vcfltl$0b#wa)%rxv=f'
+SECRET_KEY = 'secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
